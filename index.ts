12bfc640fe8fc376278c589f6674ef5d2bdb007e
Muhammad-Ahmad-Inamdar <mohammad.ainamdar@gmail.com>
2024-03-22 03:19:59 +0500

    Pushed on github first time

---
+++ index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer'
+
+const answer =await inquirer.prompt([
+    {message:'Enter first number', type:'number',name:'first_number'},
+    
+    {message:'Enter second number', type:'number',name:'second_number'},
+    
+    {message:'Chose any mathematical operator',
+    type:'list',
+    name:'operator',
+    choices:['Addition','Subtraction','Multiplication','Division']}
+
+])
+// conditional statement //
+if (answer.operator === 'Addition') {
+    console.log(`Answer is : ${answer.first_number+answer.second_number}`)
+}
+else if (answer.operator === 'Subtraction') {
+    console.log(`Answer is : ${answer.first_number-answer.second_number}`)
+}
+else if (answer.operator === 'Multiplication') {
+    console.log(`Answer is : ${answer.first_number*answer.second_number}`)
+}
+else if (answer.operator === 'Division') {
+    console.log(`Answer is : ${answer.first_number/answer.second_number}`)
+    
+}
+else {'You did not chose properly'}
